69acb421bb450790883ae41e6a70cd6bce6e6992
slcwestdevcamp <slcwest@devcamp.com>
2017-07-19 14:35:26 +0000

    Integrated new gem into rails app for copyright

--- devcamp_portfolio/app/helpers/application_helper.rb
+++ devcamp_portfolio/app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    MorganViewTool::Renderer.copyright 'Carter Morgan', 'All rights reserved'
+  end
 end
